2e5e66d9587108be975a2cd8694705415b7d4092
mdark1001 <miguel.cabrera.app@gmail.com>
2019-12-03 21:42:31 -0600

    first commit

---
+++ src/views/Crear.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <form action="">
+      <label for="name">Nombre</label>
+      <input name="name" v-model="contact.name" type="text" class="lui-input"/>
+
+    </form>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'crear_contacto',
+  data: () => ({
+    contact: {
+      name: '',
+      phone: '',
+      email: ''
+    }
+  })
+}
+</script>
